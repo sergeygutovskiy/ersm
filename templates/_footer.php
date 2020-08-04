7357e8ebac8d21fa4c630206dccd228db27be44a
sergeygutovskiy <sergeygutovsk@gmail.com>
2020-08-04 23:10:47 +0300

    footer

---
+++ templates/_footer.php
@@ -0,0 +1,35 @@
+<footer class="footer">
+	<div class="main-container footer__content">
+		<section class="footer__logo-container">
+			<img src="img/logo.png" class="footer__logo">
+		</section>
+
+		<section class="footer__center">
+			<ul class="footer__links">
+				<li class="footer__link"><a href="/">Услуги</a></li>
+				<li class="footer__link"><a href="/">Проекты</a></li>
+				<li class="footer__link"><a href="/">О нас</a></li>
+				<li class="footer__link"><a href="/">Стоимость</a></li>
+				<li class="footer__link"><a href="/">Контакты</a></li>
+			</ul>
+			<section class="footer__kirill">
+				<a href="http://terexov.agency">terexov.agency</a>
+				<div>разработка сайта</div>
+			</section>
+		</section>
+
+		<section class="footer__info">
+			<div class="footer__phone">
+				<i class="fas fa-phone-alt"></i>
+				+7 (962) 684-76-15
+			</div>
+			<div class="footer__mail">
+				<i class="fas fa-envelope"></i>
+				office@ersm-spb.ru
+			</div>
+			<div class="footer__company-name">
+				© 2020 ersm-sbp.ru <br> Ремонтно-строительная компания
+			</div>
+		</section>		
+	</div>
+</footer>
