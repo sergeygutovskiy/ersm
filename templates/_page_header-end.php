4ef2199d60c5f06c7b77eaf107625f97ef7bffe3
sergeygutovskiy <sergeygutovsk@gmail.com>
2020-08-05 22:30:33 +0300

    header fix

--- templates/_page_header-end.php
+++ templates/_page_header-end.php
@@ -4,9 +4,9 @@
 
 <script>
 	// We listen to the resize event
-	window.addEventListener('resize', () => {
+	// window.addEventListener('resize', () => {
 		// We execute the same script as before
 		let vh = window.innerHeight * 0.01;
 		document.documentElement.style.setProperty('--vh', `${vh}px`);
-	});
+	// });
 </script>
