63bb49467ec35838683c471baa1251694fd711e7
sergeygutovskiy <sergeygutovsk@gmail.com>
2020-08-06 01:39:37 +0300

    huy

--- prices.php
+++ prices.php
@@ -1,3 +1,4 @@
+	<!-- header -->
 <!DOCTYPE html>
 <html>
 <head>
@@ -139,6 +140,95 @@
 				<img class="prices-list-image" src="/img/index-backgrounds/background-3.jpg">
 			</div>
 
+			<div class="main-title">Входные группы</div>
+			<div class="prices-list-container">
+				<ul class="prices-list">
+					<li class="prices-list__item">
+						<span>Тип работ</span>
+						<span>Стоимость</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Грунтование крыльца для нанесения гранитной крошки</span>
+						<span>от 110 р. за м2</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Монтаж пристенного поручня (перила до 6 п.м)</span>
+						<span>от 1600 р. за шт</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Нанесение гранитной крошки до 15 м2</span>
+						<span>от 1300 р. за м2</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Нанесение гранитной крошки от 15 м2</span>
+						<span>от 650 р. за м2</span>
+					</li>
+				</ul>	
+				<img class="prices-list-image" src="/img/index-backgrounds/background-3.jpg">
+			</div>
+
+			<div class="main-title">Прочие работы</div>
+			<div class="prices-list-container">
+				<ul class="prices-list">
+					<li class="prices-list__item">
+						<span>Тип работ</span>
+						<span>Стоимость</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Мойка ствола мусоропровода с дезинфекцией (9 этажей)</span>
+						<span>от 7000 р.</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Монтаж шибера с герметизацией швов</span>
+						<span>от 4000 р.</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Ремонт двери металлопластиковой (петли, сендвич панели)</span>
+						<span>от 4000 р. за шт</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Ремонт двери металлической (30% от стоимости новой двери)</span>
+						<span>от 4200 р. за шт</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Демонтаж радиатора отопления</span>
+						<span>от 400 р. за шт</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Установка газонного ограждения с бетонированием</span>
+						<span>от 1600 р. за м.п.</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Окраска газонного ограждения</span>
+						<span>от 310 р. за м2</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Ремонт газонного ограждения (сварка)</span>
+						<span>от 630 р. за м.п.</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Монтаж лесов высота до 4м</span>
+						<span>от 2400 р. за шт</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Монтаж лесов высота до 6м</span>
+						<span>от 4200 р. за шт</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Доставка материалов, малогабаритных</span>
+						<span>от 1800 р.</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Доставка материалов, газель 3м</span>
+						<span>от 2300 р.</span>
+					</li>
+					<li class="prices-list__item">
+						<span>Доставка материалов, газель 4м</span>
+						<span>от 3200 р.</span>
+					</li>
+				</ul>	
+				<img class="prices-list-image" src="/img/index-backgrounds/background-3.jpg">
+			</div>
 		</section>
 	</main>
 
--- templates/_page_header-end.php
+++ templates/_page_header-end.php
@@ -3,10 +3,6 @@
 </header>
 
 <script>
-	// We listen to the resize event
-	// window.addEventListener('resize', () => {
-		// We execute the same script as before
-		let vh = window.innerHeight * 0.01;
-		document.documentElement.style.setProperty('--vh', `${vh}px`);
-	// });
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
 </script>
