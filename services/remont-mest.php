b507c663e69f203db7f13d85978470f61cb14421
maryan-ochka <45464271+maryan-ochka@users.noreply.github.com>
2020-08-07 01:58:17 +0300

    vorota-i-kalitki

--- services/remont-mest.php
+++ services/remont-mest.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Ремонт мест общего использования — ЕРСМ</title>
+	<title>Ремонт мест общего пользования — ЕРСМ</title>
 
 	<?php require "../templates/_header.php"; ?>
 
