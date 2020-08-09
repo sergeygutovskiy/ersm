5c92a9ec0f9340e909056da890eee21f37361ff1
maryan-ochka <45464271+maryan-ochka@users.noreply.github.com>
2020-08-09 16:17:59 +0300

    /services/group/blagoustrojstvo-pridomovoj-territorii

---
+++ services/group/blagoustrojstvo-pridomovoj-territorii.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Страница</title>
+
+	<?php require "../../templates/_header.php"; ?>
+
+	<link rel="stylesheet" type="text/css" href="/css/pages/services_group.css">
+	<link rel="stylesheet" type="text/css" href="/css/_header.css">
+	<link rel="stylesheet" type="text/css" href="/css/_header_all_pages.css">
+</head>
+
+<body>
+
+	<!-- page header starts here -->
+	<?php require "../../templates/_page_header-start.php"; ?>
+		
+		<img class="header-background-image" src="/img/index-backgrounds/background-1.jpg">
+		<div class="header__title header__title--common-page ">
+			Благоустройство придомовой территории
+		</div>
+
+	<?php require "../../templates/_page_header-end.php"; ?>
+	<!-- page header ends here -->
+
+	<!-- page content starts here -->
+
+	<main class="main-container main-limited">
+		
+		<section class="services-group">
+			
+
+			<article class="services-group__item size-2">
+				<img class="services-group__image" src="/img/page-headers/services/remont-mest-2.jpg">
+				<div class="services-group__background"></div>
+				<div class="services-group__title">
+					<a href="/">Ремонт мест общего пользования</a>
+				</div>
+			</article>
+
+			<article class="services-group__item size-1">
+				<img class="services-group__image" src="/img/page-headers/services/vorota-i-kalitki.jpg">
+				<div class="services-group__background"></div>
+				<div class="services-group__title">
+					<a href="/">Ворота и калитки</a>
+				</div>
+			</article>
+
+			<article class="services-group__item size-1">
+				<img class="services-group__image" src="/img/page-headers/services/ustanovka-zaborov-2.jpg">
+				<div class="services-group__background"></div>
+				<div class="services-group__title">
+					<a href="/">Установка заборов</a>
+				</div>
+			</article>
+
+			<article class="services-group__item size-1">
+				<img class="services-group__image" src="/img/page-headers/services/trotuarnaya-plitka.jpg">
+				<div class="services-group__background"></div>
+				<div class="services-group__title">
+					<a href="/">Укладка тротуарной плитки</a>
+				</div>
+			</article>
+
+			<article class="services-group__item size-1">
+				<img class="services-group__image" src="/img/page-headers/services/ustanovka-lavochek-i-urn.jpg">
+				<div class="services-group__background"></div>
+				<div class="services-group__title">
+					<a href="/">Установка лавочек и урн</a>
+				</div>
+			</article>
+
+			<article class="services-group__item size-2">
+				<img class="services-group__image" src="/img/page-headers/services/navesy-i-kozyrki-2.jpg">
+				<div class="services-group__background"></div>
+				<div class="services-group__title">
+					<a href="/">Установка навесов и козырьков над входом</a>
+				</div>
+			</article>
+
+			<article class="services-group__item size-1">
+				<img class="services-group__image" src="/img/page-headers/services/gazonnye-ograzhdeniya.jpg">
+				<div class="services-group__background"></div>
+				<div class="services-group__title">
+					<a href="/">Установка газонных ограждений</a>
+				</div>
+			</article>
+
+			<article class="services-group__item size-1">
+				<img class="services-group__image" src="/img/page-headers/services/musornye-baki.jpg">
+				<div class="services-group__background"></div>
+				<div class="services-group__title">
+					<a href="/">Площадки для мусорных баков</a>
+				</div>
+			</article>
+
+			<article class="services-group__item size-1">
+				<img class="services-group__image" src="/img/page-headers/services/gazony-i-klumby.jpg">
+				<div class="services-group__background"></div>
+				<div class="services-group__title">
+					<a href="/">Устройство газонов и клумб</a>
+				</div>
+			</article>
+
+			<article class="services-group__item size-1">
+				<img class="services-group__image" src="/img/page-headers/services/skamejki-i-urny-2.jpg">
+				<div class="services-group__background"></div>
+				<div class="services-group__title">
+					<a href="/">Скамейки, урны, цветочницы, ограничители движения</a>
+				</div>
+			</article>	
+
+		</section>
+
+	</main>
+	<!-- page content ends here -->
+
+	<!-- callback form -->
+	<?php require "../../templates/_callback.php"; ?>
+	<!-- footer -->
+	<?php require "../../templates/_footer.php"; ?>
+
+</body>
+</html>
