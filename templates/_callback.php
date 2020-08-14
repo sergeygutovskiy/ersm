a386b5f31c621019759f668a99c4ba91f320c1cd
Kirill228sexy <kirillteterehov@yandex.ru->
2020-08-14 16:00:36 +0300

    Update _callback.php

--- templates/_callback.php
+++ templates/_callback.php
@@ -5,9 +5,9 @@
 			<h3>Заполните форму, и мы вам перезвоним</h3>
 			<p>
 				Заполните специальную форму на бесплатный вызов мастера или позвоните по телефонам 
-				<br class="br-form"><a href="" class="main-text-blue">+7 (962) 684-76-15</a> 
+				<br class="br-form"><a  href="tel:+79626847615" class="main-text-blue">+7 (962) 684-76-15</a> 
 				или 
-				<br class="br-form"><a href="" class="main-text-blue">+7 (999) 238-30-08</a> 
+				<br class="br-form"><a href="tel:+79992383008" class="main-text-blue">+7 (999) 238-30-08</a> 
 				с 9:00 до 21:00.
 			</p>
 			<p>
