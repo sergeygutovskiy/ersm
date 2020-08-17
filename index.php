5c145ca21dcea47f9d8224ddc3965f0953020c06
Kirill228sexy <kirillteterehov@yandex.ru->
2020-08-18 02:22:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,17 +12,18 @@
 
 	<!-- popup -->
 
-	<section class="main-popup">
-		<div class="main-popup__image"></div>
-		<div class="main-popup__text">
-			Выезд специалистов <br>
-			на <a class="main-text-blue" href="/services/avarijnye-raboty"> аварийные работы </a>
-		</div>
-		<button class="main-popup__close" onclick="closePopup()">
-			<i class="far fa-times-circle"></i>
-		</button>
-	</section>
-
+	<a class="main-text-blue" href="/services/avarijnye-raboty">
+		<section class="main-popup">
+			<div class="main-popup__image"></div>
+			<div class="main-popup__text">
+				Выезд специалистов <br>
+				на  аварийные работы 
+			</div>
+			<button class="main-popup__close" onclick="closePopup()">
+				<i class="far fa-times-circle"></i>
+			</button>
+		</section>
+	</a>
 	<script>
 		
 		var popup = document.getElementsByClassName("main-popup")[0];
